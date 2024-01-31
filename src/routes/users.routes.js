952cc0a117c2b9cc8a7c66ea3aff3d110238a2c8
welesonbatista <weleson.dev@gmail.com>
2024-01-31 15:50:38 -0300

    add token

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,14 +1,21 @@
-const { Router } = require('express')
+const { Router, request } = require('express')
+const multer = require('multer')
+const uploadConfig = require('../configs/upload')
 
 const UsersController = require('../controllers/usersController')
+const ensureAuthenticated = require('../middlewares/ensureAuthenticated')
 
 const usersRoutes = Router()
+const upload = multer(uploadConfig.MULTER)
 
 const usersController = new UsersController()
 
 usersRoutes.post('/', usersController.create)
-usersRoutes.put('/:id', usersController.update)
-
+usersRoutes.put('/', ensureAuthenticated, usersController.update)
+usersRoutes.patch('/avatar', ensureAuthenticated, upload.single('avatar'), (request, response) => {
+    console.log(request.file.filename)
+    response.json()
+})
 
 
 module.exports = usersRoutes
