301f558790760353fd506950882ae04b6053b0a7
Weleson Batista <weleson.dev@gmail.com>
2024-07-30 19:45:49 -0300

    deploy test

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps : [{
+    name: "api-notes",
+    script: "./src/server.js",
+    instances: "max",
+    env: {
+      NODE_ENV: "development",
+    },
+    env_production: {
+      NODE_ENV: "production",
+    }
+  }]
+}
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 require('express-async-errors')
+require("dotenv/config")
 const migrationsRun = require('./database/sqlite/migrations')
-const AppError = require('./utils/appError')
+const AppError = require('./utils/AppError')
 const uploadConfig = require('./configs/upload')
 
 const cors = require('cors')
@@ -33,5 +34,5 @@ app.use((error, request, response, next) => {
     })
 })
 
-const PORT = 3333
+const PORT = process.env.PORT || 3333
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
